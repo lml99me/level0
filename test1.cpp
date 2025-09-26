6caab2c3d4e2021895fc6008a1f0e7446ea5afe1
lml99me <qhydylyxtycha@gmail.com>
2025-09-26 23:00:09 +0800

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,68 @@
+#include<stdio.h> 
+#include<string.h>
+int main()
+{
+
+    char word[ 100];
+    scanf("%s",&word);
+    if(strcmp(word,"Dian")==0)
+    printf("2002\n");
+    else if(strcmp(word,"Quit")==0)
+    return 0;
+    else
+    printf("Error")
+;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+	
+   
+    
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ test2.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	FILE*fp;
+	fp=fopen("abc.txt","r");
+	if(fp==NULL){
+		printf(" ß∞‹\n");
+		return 1; 
+	}
+	char shuru[256];
+	char cunfang[256];
+	while(1){
+	
+		printf("«Î ‰»Î£∫");
+		scanf("%s",shuru);
+		if(strcmp(shuru,"Quit")==0){
+			break;
+		}
+		rewind(fp);
+		int i=0;
+		while(fgets(cunfang,100,fp)){
+			char*p=strchr(cunfang,':');
+			if(p!=NULL){
+				char key[256];
+				strncpy(key,cunfang,p-cunfang);
+				key[p-cunfang]='\0';
+			if(strcmp(key,shuru)==0){
+				printf("%s",p + 1); 
+				i=1;
+				break;
+			}
+			
+			}
+				
+			}
+		if(!i)	{
+				printf("Error\n");}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			}
+	
+		fclose(fp);
+	return 0;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
